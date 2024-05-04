e1ccba03c256f314a25265e98e46bc1679c628a4
HanamiGeto <tidu11@hotmail.de>
2024-05-04 22:04:46 +0200

    installed prettier and configured eslint

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -4,14 +4,14 @@ import { Book } from '../shared/book';
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent {
   books: Book[] = [];
   @Output() selectBook = new EventEmitter<Book>();
 
   constructor() {
-    this.books =  [
+    this.books = [
       {
         isbn: '12345',
         title: 'Tierisch gut kochen',
@@ -19,7 +19,7 @@ export class BookListComponent {
         published: '2022-06-20',
         subtitle: 'Rezepte von Affe bis Zebra',
         thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
-        description: 'Immer lecker und gut'
+        description: 'Immer lecker und gut',
       },
       {
         isbn: '67890',
@@ -28,8 +28,8 @@ export class BookListComponent {
         published: '2022-07-15',
         subtitle: 'Bananenbrot und mehr',
         thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
-        description: 'Tolle Backtipps für Mensch und Tier'
-      }
+        description: 'Tolle Backtipps für Mensch und Tier',
+      },
     ];
   }
 

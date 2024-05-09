a4e9a994d78c2a537adf9d3aed3010b7965def94
HanamiGeto <tidu11@hotmail.de>
2024-05-09 18:35:37 +0200

    reactive forms - updated book-form component to reactive forms

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BookFormComponent } from './book-form/book-form.component';
 import { BookCreateComponent } from './book-create/book-create.component';
+import { LocalDateValueAccessor } from 'angular-date-value-accessor';
 
 @NgModule({
   declarations: [BookFormComponent, BookCreateComponent],
-  imports: [CommonModule, AdminRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    ReactiveFormsModule,
+    LocalDateValueAccessor,
+  ],
 })
 export class AdminModule {}
